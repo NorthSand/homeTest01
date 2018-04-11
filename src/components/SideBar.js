d71f645ef465680081fe4db051b58c7f4351938b
NorthSand <375058035@qq.com>
2018-04-11 21:23:59 +0800

    add

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -12,7 +12,7 @@ export default class SideBar extends Component {
                             <img src="img/user2-160x160.jpg" className="img-circle" alt="User Image" />
                         </div>
                         <div className="pull-left info">
-                            <p>Alexander Pierce</p>
+                            <p>天乐镇</p>
                             <a href="#"><i className="fa fa-circle text-success"></i> Online</a>
                         </div>
                     </div>
