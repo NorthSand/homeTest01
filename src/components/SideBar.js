5caf32803feaa7274342ab8fdb53ae8a8b8c566e
NorthSand <github.com>
2018-04-11 21:43:43 +0800

    初次修改

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -12,7 +12,7 @@ export default class SideBar extends Component {
                             <img src="img/user2-160x160.jpg" className="img-circle" alt="User Image" />
                         </div>
                         <div className="pull-left info">
-                            <p>world Tyz</p>
+                            <p>world中国</p>
                             <a href="#"><i className="fa fa-circle text-success"></i> Online</a>
                         </div>
                     </div>
