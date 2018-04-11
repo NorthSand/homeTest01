000f9b3c5cf0f3b67c5dd312b41e0360bccf4e76
NorthSand <375058035@qq.com>
2018-04-11 21:38:30 +0800

    slkj

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -12,7 +12,7 @@ export default class SideBar extends Component {
                             <img src="img/user2-160x160.jpg" className="img-circle" alt="User Image" />
                         </div>
                         <div className="pull-left info">
-                            <p>天乐镇</p>
+                            <p>world Tyz</p>
                             <a href="#"><i className="fa fa-circle text-success"></i> Online</a>
                         </div>
                     </div>
